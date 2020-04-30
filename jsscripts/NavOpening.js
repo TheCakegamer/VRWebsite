9f226dea59b1d6f687e31e61dcd92808dada038a
Tim <40315936+TheCakegamer@users.noreply.github.com>
2020-04-30 10:13:12 +0200

    Fixed text alignment

--- jsscripts/NavOpening.js
+++ jsscripts/NavOpening.js
@@ -14,4 +14,6 @@ function closeNav() {
 
 $(window).on('load', function () {
     $("#container1").twentytwenty();
+    var imgWidth = $("#container1 img").width()/2;
+    $("#container1").css({"position":"relative","left":"calc(50% - "+ imgWidth+ "px)"});
 });
