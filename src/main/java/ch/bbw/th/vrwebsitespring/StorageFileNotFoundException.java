104bcfce800cc35319b5bd6d45ba5195c31ac114
Tim <40315936+thecakegamer@users.noreply.github.com>
2020-07-02 00:01:30 +0200

    Added basic fileupload

---
+++ src/main/java/ch/bbw/th/vrwebsitespring/StorageFileNotFoundException.java
@@ -0,0 +1,13 @@
+package ch.bbw.th.vrwebsitespring;
+
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/ch/bbw/th/vrwebsitespring/StorageProperties.java
@@ -0,0 +1,18 @@
+package ch.bbw.th.vrwebsitespring;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
